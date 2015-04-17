5b5be4bdeace2b5650e68a3629152d1c90a020e3
Will <wlefevers@gmail.com>
2015-04-16 21:36:01 -0400

    Init.

---
+++ imap.py
@@ -0,0 +1,9 @@
+'''
+- Initial goals
+-- open an IMAP box
+-- Fetch messages
+-- Read headers
+-- Perform statistical analysis of IPs and URLs and decide if they're malicious
+-- Take actions based on results (respond, delete, bin, etc)
+
+'''
